5cc091f89780e1200d273ee11e7506505b7572ae
Your Name <laxmiptdr4@gmail.com>
2025-03-08 17:02:40 +0530

    alumini integration

--- Post/models.py
+++ Post/models.py
@@ -9,18 +9,9 @@ class User_Post(models.Model):
     image = models.ImageField(upload_to='post_images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    like = models.ManyToManyField(User, related_name='alumni_likes', blank=True)
 
 
     def __str__(self):
         return self.title
 
 
-class Comment(models.Model):
-    post = models.ForeignKey(User_Post, related_name='post_comments', on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.TextField(max_length=500)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.user.username}: {self.text}"
